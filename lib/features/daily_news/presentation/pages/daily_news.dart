177a1159babe3ac0d914742aec24131fdd6e3118
terfiel-22 <dietherfiel@gmail.com>
2025-03-27 21:56:09 +0800

    Pull down to refresh

--- lib/features/daily_news/presentation/pages/daily_news.dart
+++ lib/features/daily_news/presentation/pages/daily_news.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 import 'package:news_app/features/daily_news/presentation/widgets/article_tile.dart';
 
-class DailyNews extends StatelessWidget {
+class DailyNews extends StatefulWidget {
   const DailyNews({super.key});
+
+  @override
+  State<DailyNews> createState() => _DailyNewsState();
+}
+
+class _DailyNewsState extends State<DailyNews> {
+  Future<void> _refreshArticles() async {
+    context.read<RemoteArticleBloc>().add(const GetArticles());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,18 +41,27 @@ class DailyNews extends StatelessWidget {
       }
 
       if (state is RemoteArticlesError) {
-        return const Center(
-          child: Icon(Icons.refresh),
+        return Center(
+          child: IconButton(
+            onPressed: _refreshArticles,
+            icon: Icon(
+              Icons.refresh,
+              size: 50,
+            ),
+          ),
         );
       }
 
       if (state is RemoteArticlesDone) {
-        return ListView.builder(
-          itemBuilder: (context, index) {
-            final article = state.articles![index];
-            return ArticleTile(article: article);
-          },
-          itemCount: state.articles?.length,
+        return RefreshIndicator(
+          onRefresh: _refreshArticles,
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              final article = state.articles![index];
+              return ArticleTile(article: article);
+            },
+            itemCount: state.articles?.length,
+          ),
         );
       }
 
